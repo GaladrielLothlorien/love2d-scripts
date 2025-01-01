76d8366034c333b31ecec3acf987c9d614f03dde
GaladrielLothlorien <galadriel1335@outlook.com>
2025-01-01 11:19:44 -0300

    Adicionei o script para cálculos de física no Love2D

--- main.lua
+++ main.lua
@@ -1,56 +1,22 @@
-<<<<<<< HEAD
---Load assets
-function love.load()
-   --Load all assets here
-   local myfont = love.graphics.newFont(45)
+local angle = 0
+ 
+local width = 10
 
-   love.graphics.setFont(myfont)
+local height = 10
 
-   love.graphics.setColor(0, 0, 0, 225)
-
-   love.graphics.setBackgroundColor(255, 153, 0)
-end
-
---Update event
-function love.update()
-  --Do the maths logic
-end
-
---Draw display
 function love.draw()
-   --Describe how you want/whart to draw
-   love.graphics.print("Ola, Mundo", 200, 200)
-end
-=======
-function love.load()
-    --Cria a fonte
-    local myfont = love.graphics.newFont(45)
+  love.graphics.rotate(angle)
 
-    --aplica a fonte
-    love.graphics.setFont(myfont)
+  love.graphics.setColor(0, 0, 225)
 
-    --Aplica a cor
-    love.graphics.setColor(0, 0, 0, 255)
-
-    --Aplica cor de fundo
-    love.graphics.setBackgroundColor(255, 153, 0)
+  love.graphics.rectangle('fill', 300, 400, width, height)
 end
 
 function love.update(dt)
-
+  if love.keyboard.isDown('d') then
+    angle = angle + math.pi * dt
+  elseif love.keyboard.isDown('a') then
+    angle = angle - math.pi * dt
+  end
+  
 end
-
-function love.draw()
-    --Mostra mensagem na tela
-    love.graphics.print('Ola, Mundo', 0, 0)
-
-    --Desenha o circulo 1-mode 2-x pos 3-y pos 4-radius 5-argumentos
-    love.graphics.circle("fill", 200, 300, 50, 50)
-
-    --Desenha o retangulo
-    love.graphics.rectangle("fill", 300, 300, 100, 100)
-
-    --Desenha um arco
-    love.graphics.arc("fill", 450, 300, 100, math.pi/5, math.pi/2)
-end
->>>>>>> f406af6 (Primeiro commit: adicionados os scripts iniciais)
